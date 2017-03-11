eb5c3c87e836a0439f5e5ee9b06aca0f7865b80d
sutirtha1977 <sutirtha.c@gmail.com>
2017-03-10 19:25:11 -0500

    Changes to view files in Show template

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,12 +1,10 @@
 module ListingsHelper
 	def image_for(listing)
-		# if listing.image?
-		# 	image_tag(listing.image.url, class: "img-responsive", size: "320x210")
-		# else
-		# 	image_tag("om.jpg", class: "img-responsive")
-		# end	
-
-		image_tag(File.join("listings", "om.jpg"), class: "img-responsive")
+		if File.exist? Rails.root.join('app', 'assets', 'images', listing.image_loc)
+			image_tag(listing.image_loc, class: "img-responsive", size: "200x200")
+		else
+			image_tag(File.join("listings", "om.jpg"), class: "img-responsive")
+		end	
 	end
 
 	def image_for_home(listing)
--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,15 +1,15 @@
 module TeamsHelper
 	def avatar_for(team)
-		# if team.avatar?
-		# 	image_tag team.avatar.url(:thumb)
-		# else
-		# 	image_tag "default_avatar.jpg"
-		# end
-		image_tag File.join("team", "default_avatar.jpg")
+		if File.exist? Rails.root.join('app', 'assets', 'images', team.avatar_loc)
+			image_tag team.avatar_loc, class: "img-circle img-responsive", size: "200x200"
+		else
+			image_tag File.join("team", "default_avatar.jpg")
+		end
 	end
-	def avatar_for_home(member)
-		if File.exist? Rails.root.join('app', 'assets', 'images', member.avatar_loc)
-			image_tag member.avatar_loc, class: "img-circle img-responsive"
+
+	def avatar_for_home(team)
+		if File.exist? Rails.root.join('app', 'assets', 'images', team.avatar_loc)
+			image_tag team.avatar_loc, class: "img-circle img-responsive"
 		else
 			image_tag File.join("team", "default_avatar.jpg"), class: "img-circle img-responsive"
 		end	
